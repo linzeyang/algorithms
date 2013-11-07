564273ff85659a24cfac5bd32c57d5678071cee5
linzeyang <zeyanglin2013@gmail.com>
2013-11-07 18:10:11 +0000

    Implement Bubble Sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,4 +1,11 @@
 # Sorting Algorithms in Python
 
-def dummy_function():
-    return True
+def bubble_sort(test_list):
+    for i in range(0, len(test_list)-1):
+        for j in range(0, len(test_list)-1-i):
+            if test_list[j] > test_list[j + 1]:
+                temp = test_list[j]
+                test_list[j] = test_list[j + 1]
+                test_list[j + 1] = temp
+
+    return test_list
--- sorting/sorting_test.py
+++ sorting/sorting_test.py
@@ -1,1 +1,36 @@
 # unit testings for sorting algorithms in Python
+
+from unittest import TestCase, main
+
+from sorting import *
+
+
+class SortingTestCase(TestCase):
+    def test_bubble_sort(self):
+        test_list = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
+        expected_list = [2, 4, 8, 13, 14, 26, 27, 28, 33, 35]
+
+        self.assertEqual(bubble_sort(test_list), expected_list)
+
+
+    def test_bubble_sort_with_ultra_long_list(self):
+        test_list = range(10**4)
+        test_list = reverse_list(test_list)
+        expected_list = range(10**4)
+
+        self.assertEqual(bubble_sort(test_list), expected_list)
+
+
+def reverse_list(original_list):
+    length = len(original_list)
+    reversed_list = []
+
+    while length > 0:
+        reversed_list.append(original_list[length - 1])
+        length -= 1
+
+    return reversed_list
+
+
+if __name__ == '__main__':
+    main()
