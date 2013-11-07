5057fd6ed1c948f9f81b13fc77666676e8ecd9ae
linzeyang <zeyanglin2013@gmail.com>
2013-11-07 18:14:27 +0000

    use Python's swap mechanizm

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -4,8 +4,6 @@ def bubble_sort(test_list):
     for i in range(0, len(test_list)-1):
         for j in range(0, len(test_list)-1-i):
             if test_list[j] > test_list[j + 1]:
-                temp = test_list[j]
-                test_list[j] = test_list[j + 1]
-                test_list[j + 1] = temp
+                test_list[j], test_list[j + 1] = test_list[j + 1], test_list[j]
 
     return test_list
