5746919f0e1b3a087ec7fe183f4efdadc859ec3b
linzeyang <zeyanglin2013@gmail.com>
2013-11-08 19:12:18 +0000

    Add insertion sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -7,3 +7,14 @@ def bubble_sort(test_list):
                 test_list[j], test_list[j + 1] = test_list[j + 1], test_list[j]
 
     return test_list
+
+
+def insertion_sort(test_list):
+    for i in range(1, len(test_list)):
+        for j in range(i-1, -1, -1):
+            if test_list[j] > test_list[j + 1]:
+                test_list[j], test_list[j + 1] = test_list[j + 1], test_list[j]
+            else:
+                break
+
+    return test_list
--- sorting/sorting_test.py
+++ sorting/sorting_test.py
@@ -6,7 +6,7 @@ from sorting import *
 
 
 class SortingTestCase(TestCase):
-    def test_bubble_sort(self):
+    def test_bubble_sort_with_short_list(self):
         test_list = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
         expected_list = [2, 4, 8, 13, 14, 26, 27, 28, 33, 35]
 
@@ -21,6 +21,21 @@ class SortingTestCase(TestCase):
         self.assertEqual(bubble_sort(test_list), expected_list)
 
 
+    def test_insertion_sort_with_short_list(self):
+        test_list = [27, 33, 28, 4, 2, 26, 13, 35, 8, 14]
+        expected_list = [2, 4, 8, 13, 14, 26, 27, 28, 33, 35]
+
+        self.assertEqual(insertion_sort(test_list), expected_list)
+
+
+    def test_insertion_sort_with_ultra_long_list(self):
+        test_list = range(10**4)
+        test_list = reverse_list(test_list)
+        expected_list = range(10**4)
+
+        self.assertEqual(insertion_sort(test_list), expected_list)
+
+
 def reverse_list(original_list):
     length = len(original_list)
     reversed_list = []
@@ -32,5 +47,13 @@ def reverse_list(original_list):
     return reversed_list
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    import sys, unittest
+    suite = unittest.TestSuite()
+    if len(sys.argv) == 1:
+        suite = unittest.TestLoader().loadTestsFromTestCase(SortingTestCase)
+    else:
+        for test_name in sys.argv[1:]:
+            suite.addTest(SortingTestCase(test_name))
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
