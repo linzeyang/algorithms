d29be24c9789a93148d514e59925126db8859010
Zeyang Lin <zeyanglin2013@gmail.com>
2016-10-18 12:13:16 +0100

    Update sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,6 +1,10 @@
 # Sorting Algorithms in Python
 
+def dummy():
+    print("Hello world!")
+
 def bubble_sort(test_list):
+    """this is test doc-string"""
     for i in range(0, len(test_list)-1):
         for j in range(0, len(test_list)-1-i):
             if test_list[j] > test_list[j + 1]:
